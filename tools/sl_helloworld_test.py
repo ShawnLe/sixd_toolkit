d1bcb7e481df00b0b82eeec274e77455b5b4684d
shawnle <shawn_le@solomon.com.tw>
2019-06-18 10:15:35 +0800

    update revise pose and change its units

--- tools/sl_helloworld_test.py
+++ tools/sl_helloworld_test.py
@@ -1,5 +1,6 @@
 __author__ = 'shawnle'
 __email__ = 'letrungson1_at_gmail.com'
+__brief__ = 'read AP generated groundtruth poses and rgb and create *-label.png, *-depth.png'
 
 import os
 import sys
@@ -20,14 +21,20 @@ from pysixd import transform as tf
 import json
 
 # DATA_ROOT = r'D:\SL\PoseCNN\Loc_data\DUCK\POSE_iPBnet'
-DATA_ROOT = r'D:\SL\PoseCNN\Loc_data\DUCK\POSE_iPBnet'
+# DATA_ROOT = r'D:\SL\PoseCNN\Loc_data\DUCK\POSE_iPBnet'
+DATA_ROOT = '/media/shawnle/Data0/YCB_Video_Dataset/SLM_datasets/Exhibition/DUCK'
 p0 = os.path.abspath(DATA_ROOT)
 
-GEN_ROOT = r'D:\SL\Summer_2019\original_sixd_toolkit\sixd_toolkit\data\gen_data'
+# GEN_ROOT = r'D:\SL\Summer_2019\original_sixd_toolkit\sixd_toolkit\data\gen_data'
+GEN_ROOT = DATA_ROOT
 
 # model = inout.load_ply(r'D:\SL\Summer_2019\sixd_toolkit\data\sheep\textured.ply')
 # model = inout.load_ply(r'D:\SL\Summer_2019\sixd_toolkit\data\ply\rotated.ply')
-model = inout.load_ply(r'D:\SL\PoseCNN\Loc_data\DUCK\015_duck_toy\textured_m_text.ply')
+# model = inout.load_ply(r'D:\SL\PoseCNN\Loc_data\DUCK\015_duck_toy\textured_m_text.ply')
+# model = inout.load_ply('/media/shawnle/Data0/YCB_Video_Dataset/YCB_Video_Dataset/data_syn_LOV/models/015_duck_toy/textured_dense.ply')
+model = inout.load_ply('/home/shawnle/Downloads/textured.ply')
+
+print('model keys', model.keys())
 
 # meta_file = os.path.join(p0, '{:06d}'.format(0) + '-meta.json')
 
@@ -54,9 +61,9 @@ def flip_yz_image(image):
     return ret
 
 # tuning pose
-tx = -.002 # mm
+tx = -.001 # mm
 ty = -.005 
-tz = -.002
+tz = -.001
 rz = 2./180.*math.pi # rad
 
 xaxis, yaxis, zaxis = [1, 0, 0], [0, 1, 0], [0, 0, 1]
@@ -95,10 +102,10 @@ for i in range(222):
         meta = json.load(f)
 
     K = np.array(meta['intrinsic_matrix']).reshape(3,3)
-    print('K',K)
+    # print('K',K)
     poses = np.array(meta['poses']).reshape(4,4)
     R = poses[:3,:3] 
-    print ('R',R)
+    # print ('R',R)
     t =  poses[:3,3]
     t /= 1000.
     # print('t',t)
@@ -111,15 +118,37 @@ for i in range(222):
     R = Rt44[:3,:3]
     t = Rt44[:3,3]
 
-    mdl_proj = renderer.render(model, im_size, K, R, t, mode='rgb', clip_near=.3, clip_far=6., shading='flat') 
-    print("dtype", mdl_proj.dtype)
-    print("max min", np.amax(mdl_proj), np.amin(mdl_proj))    
+    mdl_proj, mdl_proj_depth = renderer.render(model, im_size, K, R, t, mode='rgb+depth', clip_near=.3, clip_far=6., shading='flat') 
+    # print("dtype", mdl_proj.dtype)
+    # print("max min", np.amax(mdl_proj), np.amin(mdl_proj))    
 
-    # mdl_proj = flip_yz_image(mdl_proj)
     # cv2.imshow('model', mdl_proj)
     # cv2.waitKey(1)    
-    mask_file = os.path.join(GEN_ROOT, '{:06d}-label.png'.format(i))
-    cv2.imwrite(mask_file, mdl_proj)
+
+    # depth format is int16
+    # convert depth (see PCNN train_net.py)
+    factor_depth = 10000
+    zfar = 6.0
+    znear = 0.25
+    im_depth_raw = factor_depth * 2 * zfar * znear / (zfar + znear - (zfar - znear) * (2 * mdl_proj_depth - 1))
+    I = np.where(mdl_proj_depth == 1)
+    im_depth_raw[I[0], I[1]] = 0
+
+    depth_file = os.path.join(GEN_ROOT, '{:06d}-depth.png'.format(i))
+    cv2.imwrite(depth_file, im_depth_raw.astype(np.uint16))
+    print('writing depth ' + depth_file)
+
+    label_file = os.path.join(GEN_ROOT, '{:06d}-label.png'.format(i))
+    # process the label image i.e. achieve nonzero pixel, then cast to cls_id value
+    I = np.where(mdl_proj_depth > 0)
+    # print('I shape',I.shape)
+    label = np.zeros((rgb.shape[0], rgb.shape[1]))
+    if len(I[0]) > 0:
+        print('len I0',len(I[0]))
+        print('label is exported')
+        label[I[0],I[1]] = 1
+    cv2.imwrite(label_file, label.astype(np.uint8))
+    print('writing label ' + label_file)
 
     blend_name = os.path.join(GEN_ROOT, "{:06d}-blend.png".format(i))
     gray = cv2.cvtColor(rgb, cv2.COLOR_BGR2GRAY)
@@ -130,4 +159,13 @@ for i in range(222):
     cv2.imwrite(blend_name,bld)
     cv2.imshow('blend', bld)
     cv2.waitKey(1)    
+    print('writing blend ' + blend_name)
+
+    # revise pose json -> unit of pose is now in meter
+    # save meta_data
+    meta_file_rev = os.path.join(p0, '{:06d}'.format(i) + '-meta_rev.json')
+    meta['poses'] = Rt44.flatten().tolist()
+    with open(meta_file_rev, 'w') as fp:
+        json.dump(meta, fp)
+    print('writing meta ',meta_file_rev) 
 
